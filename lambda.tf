3deb76eedd926fc358139d426862392344523a1f
“OjoOluwagbenga700” <ojooluwagbenga700@gmail.com>
2025-11-02 19:21:08 +0100

    terraform codes update

--- lambda.tf
+++ lambda.tf
@@ -1,23 +1,3 @@
-# -------------------------------
-# AWS Lambda Role
-# -------------------------------
-resource "aws_iam_role" "lambda_exec_role" {
-  name = "clinica_lambda_exec_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-        Effect = "Allow"
-        Sid    = ""
-      }
-    ]
-  })
-}
 
 # ======================================================
 # KMS Key for ClinicaVoice Encryption & Compliance
@@ -54,67 +34,84 @@ resource "aws_kms_alias" "clinica_key_alias" {
   target_key_id = aws_kms_key.clinica_key.id
 }
 
-# This data block helps reference your AWS Account ID dynamically
-data "aws_caller_identity" "current" {}
-
-# -------------------------------
-# Attach policy for CloudWatch and S3 access
-# -------------------------------
-resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
-  role       = aws_iam_role.lambda_exec_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_s3_access" {
-  role       = aws_iam_role.lambda_exec_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-}
 
 # -------------------------------
 # Lambda Function
 # -------------------------------
-resource "aws_lambda_function" "clinica_lambda" {
-  function_name = "clinica-voice-processor"
-  role          = aws_iam_role.lambda_exec_role.arn
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.9"
-  filename      = "lambda.zip"
-  timeout       = 10
-  memory_size   = 128
+data "archive_file" "transcribe_zip" {
+  type        = "zip"
+  source_file = "lambda_transcribe.py"
+  output_path = "transcribe.zip"
+}
 
+resource "aws_lambda_function" "transcribe_lambda_function" {
+  filename         = data.archive_file.transcribe_zip.output_path
+  function_name    = "clinica-voice-processor"
+  role             = aws_iam_role.lambda_exec_role.arn
+  handler          = "lambda_transcribe.lambda_handler"
+  source_code_hash = filebase64sha256(data.archive_file.transcribe_zip.output_path)
+  runtime          = "python3.9"
   environment {
     variables = {
-      STAGE = "prod"
+      DYNAMO_TABLE = aws_dynamodb_table.clinica_metadata_table.name
+      BUCKET_NAME  = aws_s3_bucket.clinica_voice_bucket.bucket
     }
   }
 }
 
-resource "aws_lambda_function" "clinica_comprehend_lambda" {
-  function_name = "clinica-comprehend-medical"
-  role          = aws_iam_role.lambda_exec_role.arn
-  handler       = "lambda_comprehend.lambda_handler"
-  runtime       = "python3.9"
-  filename      = "lambda_comprehend.zip"
-  timeout       = 60
+#resource "aws_lambda_function" "clinica_lambda" {
+#function_name = "clinica-voice-processor"
+#role          = aws_iam_role.lambda_exec_role.arn
+#handler       = "lambda_function.lambda_handler"
+#runtime       = "python3.9"
+#filename      = "lambda.zip"
+#timeout       = 10
+#memory_size   = 128
 
+#environment {
+#variables = {
+#STAGE = "prod"
+#}
+#}
+#}
+
+
+
+data "archive_file" "comprehend_zip" {
+  type        = "zip"
+  source_file = "lambda_comprehend.py"
+  output_path = "comprehend.zip"
+}
+
+resource "aws_lambda_function" "comprehend_lambda_function" {
+  filename         = data.archive_file.comprehend_zip.output_path
+  function_name    = "clinica-voice-comprehend"
+  role             = aws_iam_role.lambda_exec_role.arn
+  handler          = "lambda_comprehend.lambda_handler"
+  source_code_hash = filebase64sha256(data.archive_file.comprehend_zip.output_path)
+  runtime          = "python3.9"
   environment {
     variables = {
-      REGION = "us-east-1"
+      DYNAMO_TABLE = aws_dynamodb_table.clinica_metadata_table.name
+      BUCKET_NAME  = aws_s3_bucket.clinica_voice_bucket.bucket
     }
   }
 }
 
-resource "aws_iam_policy_attachment" "comprehend_basic_policy" {
-  name       = "comprehend-basic-policy"
-  roles      = [aws_iam_role.lambda_exec_role.name]
-  policy_arn = "arn:aws:iam::aws:policy/ComprehendFullAccess"
-}
+#resource "aws_lambda_function" "comprehend_lambda" {
+#function_name = "clinica-comprehend-medical"
+#role          = aws_iam_role.lambda_exec_role.arn
+#handler       = "lambda_comprehend.lambda_handler"
+#runtime       = "python3.9"
+#filename      = "lambda_comprehend.zip"
+#timeout       = 60
 
-resource "aws_iam_policy_attachment" "comprehend_s3_policy" {
-  name       = "comprehend-s3-policy"
-  roles      = [aws_iam_role.lambda_exec_role.name]
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-}
+#environment {
+#variables = {
+# REGION = "us-east-1"
+# }
+#}
+#}
 
 # ---------------------------------------
 # S3 Trigger for Comprehend Lambda
@@ -122,7 +119,7 @@ resource "aws_iam_policy_attachment" "comprehend_s3_policy" {
 resource "aws_lambda_permission" "allow_s3_to_invoke_comprehend" {
   statement_id  = "AllowS3InvokeComprehend"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.clinica_comprehend_lambda.function_name
+  function_name = aws_lambda_function.comprehend_lambda_function.function_name
   principal     = "s3.amazonaws.com"
   source_arn    = aws_s3_bucket.clinica_voice_bucket.arn
 }
@@ -131,7 +128,7 @@ resource "aws_s3_bucket_notification" "trigger_comprehend_lambda" {
   bucket = aws_s3_bucket.clinica_voice_bucket.id
 
   lambda_function {
-    lambda_function_arn = aws_lambda_function.clinica_comprehend_lambda.arn
+    lambda_function_arn = aws_lambda_function.comprehend_lambda_function.arn
     events              = ["s3:ObjectCreated:*"]
     filter_suffix       = ".json" # triggers only for JSON output files
   }
